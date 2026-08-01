a42326f23757b4954fbb22a375e85505d39736d2
Bhargav Sriram Siddani <siddanibhargav@gmail.com>
2026-07-31 17:31:18 -0700

    Commented out ad-hoc regularization to higher-order viscosity and some VOF ASCII outputs

--- src/incflo.cpp
+++ src/incflo.cpp
@@ -168,11 +168,11 @@ void incflo::Evolve()
                 printGridSummary(amrex::OutStream(), 0, finest_level);
             }
         }
-        if(m_vof_advect_tracer){
-          get_volume_of_fluid()->output_droplet(m_cur_time,m_nstep);
-          //if (m_nstep<10)
-          //get_volume_of_fluid()->apply_velocity_field(m_cur_time,m_nstep);
-        }
+        //if(m_vof_advect_tracer){
+        //  get_volume_of_fluid()->output_droplet(m_cur_time,m_nstep);
+        //  //if (m_nstep<10)
+        //  //get_volume_of_fluid()->apply_velocity_field(m_cur_time,m_nstep);
+        //}
         //if (writeNow()&& m_vof_advect_tracer){
         //    get_volume_of_fluid()->WriteTecPlotFile (finest_level,m_cur_time,m_nstep);
         //    get_volume_of_fluid()->write_tecplot_surface(finest_level,m_cur_time,m_nstep);
